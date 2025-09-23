8ed9cf522e46b5d7a423147baed65e18a2fe6b97
elite271 <20652534+elite271@users.noreply.github.com>
2025-09-22 19:26:47 -0600

    Added HexSearchEngine test

--- hexeditor/tests/tst_hexsearchengine.cpp
+++ hexeditor/tests/tst_hexsearchengine.cpp
@@ -1,2 +1,57 @@
 #include <QtTest>
 #include "HexSearchEngine.h"
+
+
+
+
+class TestHexSearchEngine : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testSetPattern();
+    void testFindHex();
+    void testFindText();
+    void testFindAll();
+};
+
+
+
+void TestHexSearchEngine::testSetPattern()
+{
+    HexSearchEngine engine;
+    engine.setPattern(QByteArray::fromHex("DEADBEEF"));
+    QCOMPARE(engine.findHex(QByteArray::fromHex("00DEADBEEF11")), qint64(2));
+}
+
+void TestHexSearchEngine::testFindHex()
+{
+    HexSearchEngine engine;
+    engine.setPattern(QByteArray::fromHex("AA"));
+    QByteArray data = QByteArray::fromHex("00AAFFAA");
+    QCOMPARE(engine.findHex(data, 0), qint64(1));
+    QCOMPARE(engine.findHex(data, 2), qint64(5));
+    QCOMPARE(engine.findHex(data, 6), qint64(-1)); // not found
+}
+
+void TestHexSearchEngine::testFindText()
+{
+    HexSearchEngine engine;
+    engine.setPattern("World");
+    QByteArray data = "Hello World!";
+    QCOMPARE(engine.findText("World"), qint64(6));
+}
+
+void TestHexSearchEngine::testFindAll()
+{
+    HexSearchEngine engine;
+    engine.setPattern(QByteArray::fromHex("AA"));
+    QByteArray data = QByteArray::fromHex("00AA11AA22");
+    QVector<qint64> matches = engine.findAll(data);
+    QCOMPARE(matches.size(), 2);
+    QCOMPARE(matches[0], qint64(1));
+    QCOMPARE(matches[1], qint64(4));
+}
+
+QTEST_MAIN(TestHexSearchEngine)
+#include "tst_hexsearchengine.moc"

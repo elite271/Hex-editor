430b624a732f3a2a48a50e3bcfee8289152b7dfd
elite <20652534+elite271@users.noreply.github.com>
2025-09-24 14:32:14 -0600

    Added test definition for hexcursor

--- hexeditor/tests/ui/tst_hexcursor.cpp
+++ hexeditor/tests/ui/tst_hexcursor.cpp
@@ -8,8 +8,72 @@ class TestHexCursor : public QObject
     Q_OBJECT
 
 private slots:
-
+  
+  void testpositionByte();
+  void testpositionNibble();
+  void testanchor();
+  void testmode();
+  void testmoveLeft();
+  void testmoveRight();
+  void testmoveUp();
+  void testmoveDown();
+  void testhasSelection();
 };
+
+
+
+  void TestHexCursor::testpositionByte()
+  {
+
+  }
+
+
+  void TestHexCursor::testpositionNibble()
+  {
+
+  }
+
+
+  void TestHexCursor::testanchor()
+  {
+
+  }
+
+
+  void TestHexCursor::testmode()
+  {
+
+  }
+
+
+  void TestHexCursor::testmoveLeft()
+  {
+
+  }
+
+
+  void TestHexCursor::testmoveRight()
+  {
+
+  }
+
+
+  void TestHexCursor::testmoveUp()
+  {
+
+  }
+
+
+  void TestHexCursor::testmoveDown()
+  {
+
+  }
+
+
+  void TestHexCursor::testhasSelection()
+  {
+    
+  }
 
 
 

cd4eea380ed387f712352f22e1c4acc5cc8fccc2
elite271 <20652534+elite271@users.noreply.github.com>
2025-09-25 16:23:03 -0600

    Removed qmake and added class declarations

---
+++ hexeditor/tests/core/tst_hexundostack.cpp
@@ -0,0 +1,12 @@
+#include <QtTest>
+#include "HexUndoStack.h"
+
+
+class TestHexUndoStack : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+
+};
--- hexeditor/tests/ui/tst_hexview.cpp
+++ hexeditor/tests/ui/tst_hexview.cpp
@@ -1,2 +1,13 @@
 #include <QtTest>
 #include "HexView.h"
+
+
+class TestHexView : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+
+
+};

8b3efc98c60532937a9f15ff1f747b22168c98aa
elite271 <20652534+elite271@users.noreply.github.com>
2025-09-28 13:14:46 -0600

    Added File explorer class

---
+++ hexeditor/ui/HexFileExplorer.h
@@ -0,0 +1,17 @@
+#ifndef HEXFILEEXPLORER_H
+#define HEXFILEEXPLORER_H
+
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QDir>
+
+class HexFileExplorer 
+{
+    public:
+
+    private:
+};
+
+#endif
